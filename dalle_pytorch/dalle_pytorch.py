1b365fea1ec66019004b6741e9de994bb2be7688
Aleksandr Borzunov <hxrussia@gmail.com>
2022-02-18 00:10:27 +0000

    Filter logits using top_k and top_p

--- dalle_pytorch/dalle_pytorch.py
+++ dalle_pytorch/dalle_pytorch.py
@@ -68,6 +68,46 @@ def top_k(logits, thres = 0.5):
     probs.scatter_(1, ind, val)
     return probs
 
+def top_k_top_p_filtering(
+    logits: torch.Tensor,
+    top_k: int = 0,
+    top_p: float = 1.0,
+    filter_value: float = -float("Inf"),
+    min_tokens_to_keep: int = 1,
+) -> torch.Tensor:
+    """ Filter a distribution of logits using top-k and/or nucleus (top-p) filtering
+        Args:
+            logits: logits distribution shape (batch size, vocabulary size)
+            if top_k > 0: keep only top k tokens with highest probability (top-k filtering).
+            if top_p < 1.0: keep the top tokens with cumulative probability >= top_p (nucleus filtering).
+                Nucleus filtering is described in Holtzman et al. (http://arxiv.org/abs/1904.09751)
+            Make sure we keep at least min_tokens_to_keep per batch example in the output
+        From: https://gist.github.com/thomwolf/1a5a29f6962089e871b94cbd09daf317
+    """
+    if top_k > 0:
+        top_k = min(max(top_k, min_tokens_to_keep), logits.size(-1))  # Safety check
+        # Remove all tokens with a probability less than the last token of the top-k
+        indices_to_remove = logits < torch.topk(logits, top_k)[0][..., -1, None]
+        logits[indices_to_remove] = filter_value
+
+    if top_p < 1.0:
+        sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+        cumulative_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
+
+        # Remove tokens with cumulative probability above the threshold (token with 0 are kept)
+        sorted_indices_to_remove = cumulative_probs > top_p
+        if min_tokens_to_keep > 1:
+            # Keep at least min_tokens_to_keep (set to min_tokens_to_keep-1 because we add the first one below)
+            sorted_indices_to_remove[..., :min_tokens_to_keep] = 0
+        # Shift the indices to the right to keep also the first token above the threshold
+        sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
+        sorted_indices_to_remove[..., 0] = 0
+
+        # scatter sorted tensors to original indexing
+        indices_to_remove = sorted_indices_to_remove.scatter(1, sorted_indices, sorted_indices_to_remove)
+        logits[indices_to_remove] = filter_value
+    return logits
+
 class SharedEmbedding(nn.Embedding):
     def __init__(self, linear, start_index, end_index, **kwargs):
         super().__init__(end_index - start_index, linear.weight.shape[1], **kwargs)
@@ -494,8 +534,9 @@ class DALLE(nn.Module):
         text,
         *,
         clip = None,
-        filter_thres = 0.5,
         temperature = 1.,
+        top_k = 0,
+        top_p = 0.0,
         img = None,
         num_init_img_tokens = None,
         cond_scale = 1.,
@@ -539,7 +580,7 @@ class DALLE(nn.Module):
 
             logits = logits[:, -1, :]
 
-            filtered_logits = top_k(logits, thres = filter_thres)
+            filtered_logits = top_k_top_p_filtering(logits, top_k = top_k, top_p = top_p)
             sample = gumbel_sample(filtered_logits, temperature = temperature, dim = -1)
 
             sample -= (num_text_tokens if is_image else 0) # offset sampled token if it is an image token, since logit space is composed of text and then image tokens
